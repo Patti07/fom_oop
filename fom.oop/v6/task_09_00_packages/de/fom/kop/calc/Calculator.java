a96dfa1bffb8298653665ee6373f3feb7021d97f
Patti07 <39049760+Patti07@users.noreply.github.com>
2021-04-20 12:04:28 +0200

    Add files via upload

---
+++ fom.oop/v6/task_09_00_packages/de/fom/kop/calc/Calculator.java
@@ -0,0 +1,13 @@
+package fom.oop.v6.task_09_00_packages.de.fom.kop.calc;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public double add(double a, double b) {
+        return a+b;
+    }
+
+}
